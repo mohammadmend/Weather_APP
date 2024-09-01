499f708d3bbf8d19822ed33b697ce43a86685938
mohammad <mohammadmend@hotmail.com>
2024-09-01 03:07:37 -0700

    With information

--- Weather_App/ContentView.swift
+++ Weather_App/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 struct ContentView: View {
     @State private var city: String = ""
     @State private var navigateToCityView = false
-    
+    @State private var showTooltip = false
+
     var body: some View {
         NavigationStack {
-            ZStack{
+            ZStack {
                 LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .bottomTrailing, endPoint: .topTrailing)
                     .edgesIgnoringSafeArea(.all)
+                
                 VStack {
+                    Text("By: Mohammad Mendahawi")
+                    
+                    Button(action: {
+                        showTooltip.toggle()
+                      
+                    }) {
+                        Image(systemName: "info.circle")
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    .overlay(
+                        Group{
+                            
+                            if showTooltip{
+                                Text("PM Accelerator is the premier AI learning and development hub, featuring award-winning AI products and mentors from top-tier companies such as Google, Meta, and Apple. We offer a dynamic AI PM Bootcamp, designed to empower the next generation of AI professionals through hands-on experience, mentorship, and real-world projects.").frame(minWidth:370,minHeight: 180).background(.white).offset(y:-150).opacity(0.2)
+                            }
+                        }
+                                            
+                    )
                     TextField("Enter City, State", text: $city)
                         .padding()
                         .background(Color.gray.opacity(0.2))
@@ -36,11 +59,11 @@ struct ContentView: View {
                     .hidden()
                     .navigationTitle("Please pick City,State")
                 }
-                
             }
         }
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
